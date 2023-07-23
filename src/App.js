816733ac54abb65db1bcf385e982461151a70909
Zarel <zarelProgramming851815@gmail.com>
2023-07-24 00:41:20 +0530

    Cat Fact v0.6

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  let [catFact, setCatFact] = useState('');
+
+  const generateNewFact = () => {
+    fetch('https://catfact.ninja/fact')
+      .then(response => response.json())
+      .then(data => setCatFact(data.fact));
+  }
+
+  useEffect(() => {
+    generateNewFact();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="fact-box">
+        <h2 className='app-name'>Cat-<span>Fact</span></h2>
+        <p className='fetched-fact'> {/* Cat-Fact is generated here */} {catFact} </p>
+        <button
+          className='generate-button'
+          onClick={generateNewFact}>
+          Generate Cat-Fact
+        </button>
+      </div>
+      <small className='credits'>Designed and Developed by Shaheer</small>
     </div>
-  );
+  )
 }
 
 export default App;
